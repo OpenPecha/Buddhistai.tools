d86808bd4a3f3745af9968b37ffbeca29a442953
tenkus47 <tenkus47@gmail.com>
2025-12-26 13:55:41 +0530

    update dependencies: upgrade @auth0/nextjs-auth0 to version 4.14.0, next to version 15.2.8, and oauth4webapi to version 3.8.3; remove deprecated configurations in auth0 setup

--- src/lib/auth0.ts
+++ src/lib/auth0.ts
@@ -1,16 +1,5 @@
-import { Auth0Client ,filterDefaultIdTokenClaims} from "@auth0/nextjs-auth0/server";
-
-
-
+import { Auth0Client } from "@auth0/nextjs-auth0/server";
 export const auth0 = new Auth0Client({
-    signInReturnToPath: "/api/auth/post-login",
-    async beforeSessionSaved(session, idToken) {
-      return {  
-        ...session,
-        user: {
-          ...filterDefaultIdTokenClaims(session.user),
-          foo: session.user.foo // keep the foo claim
-        }
-      };
-    }
-  });
+  useDPoP: true
+  // Keys loaded automatically from environment variables
+});
